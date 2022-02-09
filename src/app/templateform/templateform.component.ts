536d9ecc041ccab461121cfec0fc169228839d72
tushar-tagline <tushar.tagline@gmail.com>
2022-02-09 18:44:39 +0530

    ng ganerate component

---
+++ src/app/templateform/templateform.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-templateform',
+  templateUrl: './templateform.component.html',
+  styleUrls: ['./templateform.component.scss']
+})
+export class TemplateformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
